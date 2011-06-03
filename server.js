5a01c37faa08554539840c114b93084a35ee89af
Ivan Shumkov <ivan@shumkov.ru>
2011-06-03 19:17:27 +0400

    fix convert and add sutter

--- server.js
+++ server.js
@@ -189,24 +189,24 @@ function deleteAllImagesRequest(request, response) {
 
 function convertOriginal(url, name, callback) {
 	im.convert([
-		'-resize', '1280x1024', url, '-sampling-factor', '2x1',  '-support', '0.9',
-		'-resize', '1280x1024', '-format', '"%w %h %[EXIF:DateTime]"',
-		'-identify', '-quality','90', name
+		'-size', '1280x1024', url, '-sampling-factor', '2x1',  '-support', '0.9',
+		'-resize', '1280x1024>', '-format', '"%w %h %[EXIF:DateTime]"',
+		'-identify', '-quality','90', '-auto-orient', name
 	], callback);
 }
 
 function convertPreview(url, name, callback) {
 	im.convert([
-		'-resize', '150x200', url, '-sampling-factor','2x1', '-support', '0.9',
-		'-resize', '150x200', '-format', '"%w %h %[EXIF:DateTime]"',
-		'-identify', '-quality', '90', name
+		'-size', '150x200', url, '-sampling-factor','2x1', '-support', '0.9',
+		'-resize', '150x200>', '-format', '"%w %h %[EXIF:DateTime]"',
+		'-identify', '-quality', '90', '-auto-orient', name
 	], callback);
 }
 
 function convertThumb(url, name, callback) {
 	im.convert([
-		'-resize', '76x76', url, '-thumbnail','76x76^', '-unsharp','0x1+1+0',
-        '-quality','90', '-flatten', '-gravity','center', '-extent', '76x76',
+		'-size', '76x76', url, '-thumbnail','76x76^', '-unsharp','0x1+1+0',
+        '-quality','90', '-auto-orient',  '-flatten', '-gravity','center', '-extent', '76x76',
         name
 	], callback);
 }
